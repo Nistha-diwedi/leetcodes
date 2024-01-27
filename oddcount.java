f88a1f090f8c2a60fb0890955538975ca76f644c
Nistha Diwedi <118626112+Nistha-diwedi@users.noreply.github.com>
2024-01-27 23:17:52 +0530

    Create oddcount.java

---
+++ oddcount.java
@@ -0,0 +1,11 @@
+class oddcount {
+    public int countOdds(int low, int high) {
+
+        if(low%2==0 & high%2==0)
+            return (high-low) /2;
+        else if(low%2==1 & high%2==1)
+            return (high-low) /2+1;
+        else
+            return (high-low+1) /2;
+    }
+}
